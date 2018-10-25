258ef6f3ce629f334438b5fe8e62e076cd2ea273
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-10-25 13:04:38 +0545

    Added some options and flags

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate clap;
 
-use clap::{App, Arg,ArgMatches};
+use clap::{App, Arg, ArgMatches};
 
 fn main() {
     let app = App::new("Cargo Cache")
@@ -11,14 +11,59 @@ fn main() {
             Arg::with_name("all")
                 .short("a")
                 .long("all")
-                .help("Clean up all .cargo/registry")
-                .takes_value(false),
+                .help("Clean up all .cargo/registry"),
+        )
+        .arg(
+            Arg::with_name("config")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("Set a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("set directory")
+                .short("s")
+                .long("set-directory")
+                .value_name("Directory")
+                .help("Set directory of Rust project")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("orphan clean")
+                .short("O")
+                .long("orphan-clean")
+                .help("Clean orphan cache crates"),
+        )
+        .arg(
+            Arg::with_name("Old clean")
+                .short("o")
+                .long("old-clean")
+                .help("Clean old cache crates"),
+        )
+        .arg(
+            Arg::with_name("exclude")
+                .short("e")
+                .long("exclude")
+                .help("Exculde listed crates")
+                .multiple(true)
+                .takes_value(true)
+                .value_name("Crate"),
+        )
+        .arg(
+            Arg::with_name("include")
+                .short("i")
+                .long("include")
+                .help("Include listed crates")
+                .multiple(true)
+                .takes_value(true)
+                .value_name("Crate"),
         )
         .get_matches();
-        matches(app);
+    matches(app);
 }
 
-fn matches(app : ArgMatches){
-    let full_clean = app.is_present("all");
-    println!("{:?}",full_clean);
+fn matches(app: ArgMatches) {
+    let _full_clean = app.is_present("all");
+    let _config_file = app.value_of("config").unwrap_or(".cargo_cache.conf");
 }
