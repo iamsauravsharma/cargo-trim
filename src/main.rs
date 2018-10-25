fa85914d4998c886ce48597642ee4f82d3ef2be3
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-10-25 12:26:42 +0545

    Add Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate clap;
+
+use clap::{App, Arg,ArgMatches};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("Cargo Cache")
+        .version("0.1.0")
+        .author("Saurav Sharma <appdroiddeveloper@gmail.com>")
+        .about("Clean cache from .cargo/registry")
+        .arg(
+            Arg::with_name("all")
+                .short("a")
+                .long("all")
+                .help("Clean up all .cargo/registry")
+                .takes_value(false),
+        )
+        .get_matches();
+        matches(app);
+}
+
+fn matches(app : ArgMatches){
+    let full_clean = app.is_present("all");
+    println!("{:?}",full_clean);
 }
