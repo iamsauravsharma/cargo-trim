e0230b18808b3042fd8e22df2bc553c785e3ff3d
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-10-28 16:32:16 +0545

    Support all directory clean

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 extern crate clap;
 extern crate dirs;
+extern crate find_folder;
 
-use std::fs::File;
+use std::fs;
 
 mod create_app;
 
 fn main() {
     let config_dir = dirs::config_dir().unwrap();
-    let home_dir = dirs::home_dir().unwrap();
+    let mut home_dir = dirs::home_dir().unwrap();
+    home_dir.push(".cargo");
+    home_dir.push("registry");
     let cargo_cache_config = format!(
         "{}/{}",
         config_dir.to_str().unwrap(),
         "cargo_cache_config.txt"
     );
-    let _registry_folder = format!("{}/.cargo/registry", home_dir.to_str().unwrap());
-    let _ = File::open(&cargo_cache_config)
-        .unwrap_or_else(|_| File::create(&cargo_cache_config).unwrap());
-    create_app::app();
+    let _ = fs::File::open(&cargo_cache_config)
+        .unwrap_or_else(|_| fs::File::create(&cargo_cache_config).unwrap());
+    let app = create_app::app();
+    if app.is_present("all") {
+        fs::remove_dir_all(home_dir).unwrap();
+    }
 }
