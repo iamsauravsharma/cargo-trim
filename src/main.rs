d2bf2abd65d95622db2cfe074ac4f021f679a59d
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-10-29 10:39:44 +0545

    Add set directory support

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ extern crate dirs;
 extern crate find_folder;
 
 use std::fs;
+use std::io::prelude::*;
 
 mod create_app;
 
 fn main() {
-    let config_dir = dirs::config_dir().unwrap();
+    let mut config_dir = dirs::config_dir().unwrap();
     let mut home_dir = dirs::home_dir().unwrap();
     home_dir.push(".cargo");
     home_dir.push("registry");
@@ -16,10 +17,21 @@ fn main() {
         config_dir.to_str().unwrap(),
         "cargo_cache_config.txt"
     );
-    let _ = fs::File::open(&cargo_cache_config)
+    let mut file = fs::File::open(&cargo_cache_config)
         .unwrap_or_else(|_| fs::File::create(&cargo_cache_config).unwrap());
+    config_dir.push("cargo_cache_config.txt");
     let app = create_app::app();
     if app.is_present("all") {
         fs::remove_dir_all(home_dir).unwrap();
+    }
+    if app.is_present("set directory") {
+        let directory = app.value_of("set directory").unwrap();
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer).unwrap();
+        if !buffer.is_empty() {
+            buffer.push_str(format!("\n").as_str());
+        }
+        buffer.push_str(format!("directory:{}", directory).as_str());
+        fs::write(config_dir, buffer.as_bytes()).unwrap();
     }
 }
