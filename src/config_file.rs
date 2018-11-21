8eeb8e109c6186dbaf1738ffd4f2beb070dd8642
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-11-21 11:49:51 +0545

    Split files

---
+++ src/config_file.rs
@@ -0,0 +1,95 @@
+use serde_derive::{Deserialize, Serialize};
+use std::{fs, io::Read, path::PathBuf};
+
+// Stores config file information
+#[derive(Serialize, Deserialize)]
+pub struct ConfigFile {
+    directory: Vec<String>,
+    include: Vec<String>,
+    exclude: Vec<String>,
+}
+
+impl ConfigFile {
+    // Create new config file
+    pub(super) fn new() -> ConfigFile {
+        ConfigFile {
+            directory: Vec::new(),
+            include: Vec::new(),
+            exclude: Vec::new(),
+        }
+    }
+
+    pub(super) fn get_directory(&self) -> Vec<String> {
+        self.directory.to_owned()
+    }
+
+    pub(super) fn get_include(&self) -> Vec<String> {
+        self.include.to_owned()
+    }
+
+    pub(super) fn get_exclude(&self) -> Vec<String> {
+        self.exclude.to_owned()
+    }
+}
+
+// Function to modify config file or read config file
+pub(super) fn modify_config_file(
+    file: &mut fs::File,
+    app: &clap::ArgMatches,
+    config_dir: &PathBuf,
+) -> ConfigFile {
+    let mut buffer = String::new();
+    file.read_to_string(&mut buffer).unwrap();
+    if buffer.is_empty() {
+        let initial_config = ConfigFile::new();
+        let serialize = serde_json::to_string(&initial_config).unwrap();
+        buffer.push_str(&serialize)
+    }
+    let mut deserialized: ConfigFile = serde_json::from_str(&buffer).unwrap();
+
+    for &name in &["set directory", "exclude-conf", "include-conf"] {
+        if app.is_present(name) {
+            let value = app.value_of(name).unwrap();
+            if name == "set directory" {
+                deserialized.directory.push(value.to_string());
+            }
+            if name == "exclude-conf" {
+                deserialized.exclude.push(value.to_string());
+            }
+            if name == "include-conf" {
+                deserialized.include.push(value.to_string());
+            }
+        }
+    }
+
+    if app.is_present("remove") {
+        let subcommand = app.subcommand_matches("remove").unwrap();
+        for &name in &["directory", "exclude", "include"] {
+            if subcommand.is_present(name) {
+                let value = subcommand.value_of(name).unwrap().to_string();
+                if name == "directory" {
+                    remove_item_crate(&mut deserialized.directory, &value);
+                }
+                if name == "exclude" {
+                    remove_item_crate(&mut deserialized.exclude, &value);
+                }
+                if name == "include" {
+                    remove_item_crate(&mut deserialized.include, &value);
+                }
+            }
+        }
+    }
+
+    let serialized = serde_json::to_string(&deserialized).unwrap();
+    buffer.clear();
+    buffer.push_str(&serialized);
+    fs::write(config_dir, buffer).unwrap();
+    deserialized
+}
+
+fn remove_item_crate(data: &mut Vec<String>, value: &str) {
+    let action = data.remove_item(&value.to_string());
+    if action.is_some() {
+        remove_item_crate(data, value);
+    }
+}
---
+++ src/git_dir.rs
@@ -0,0 +1,58 @@
+use std::{fs, path::Path};
+
+// Stores .cargo/registry cache & src information
+pub struct GitDir {
+    git_cache_dir: String,
+    git_src_dir: String,
+}
+
+impl GitDir {
+    // Create new GitDir
+    pub(super) fn new(cache_dir: &Path, src_dir: &Path) -> GitDir {
+        let git_cache_dir = open_github_folder(&cache_dir).unwrap();
+        let git_src_dir = open_github_folder(&src_dir).unwrap();
+        GitDir {
+            git_cache_dir,
+            git_src_dir,
+        }
+    }
+
+    // Remove crate from src & cache directory
+    pub(super) fn remove_crate(&self, crate_name: &str) {
+        remove_crate(Path::new(&self.git_cache_dir), crate_name);
+        remove_crate(Path::new(&self.git_src_dir), crate_name);
+    }
+
+    pub(super) fn get_src(&self) -> &String {
+        &self.git_src_dir
+    }
+}
+
+// Use to open github folder present inside src and cache folder
+fn open_github_folder(path: &Path) -> Option<String> {
+    for entry in fs::read_dir(path).unwrap() {
+        let path = entry.unwrap().path();
+        let path = path.to_str().unwrap();
+        if path.contains("github.com") {
+            return Some(path.to_string());
+        }
+    }
+    None
+}
+
+// Remove crates which name is provided to delete
+fn remove_crate(path: &Path, value: &str) {
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.to_str().unwrap().contains(value) {
+            if path.is_file() {
+                fs::remove_file(path).unwrap();
+            } else if path.is_dir() {
+                fs::remove_dir_all(path).unwrap();
+            }
+        } else if path.is_dir() {
+            remove_crate(&path, value);
+        }
+    }
+}
