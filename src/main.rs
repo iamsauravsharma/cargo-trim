163e4d85c0bd44fba24f1224bead62a69ed3aa3c
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-11-23 23:04:35 +0545

    Move test mod to different file

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod config_file;
 mod create_app;
 mod create_dir;
 mod git_dir;
+#[cfg(test)]
+mod test;
 
 use fs_extra::dir;
 use std::{
@@ -240,56 +242,4 @@ fn remove_version(installed_crate: &[String]) -> Vec<String> {
         removed_version.push(data);
     }
     removed_version
-}
-
-#[cfg(test)]
-mod test {
-    use std::{io::Read, process::Command};
-    #[test]
-    fn test_help() {
-        if !cfg!(target_os = "windows") {
-            let output = Command::new("sh")
-                .arg("-c")
-                .arg("cargo run -- -h")
-                .output()
-                .expect("failed to execute process");
-            let output = String::from_utf8(output.stdout).unwrap();
-            let mut buffer = String::new();
-            let mut file = std::fs::File::open("tests/command_output/help.txt").unwrap();
-            file.read_to_string(&mut buffer).unwrap();
-            assert_eq!(output, buffer);
-        }
-    }
-
-    #[test]
-    fn test_list_help() {
-        if !cfg!(target_os = "windows") {
-            let output = Command::new("sh")
-                .arg("-c")
-                .arg("cargo run -- help list")
-                .output()
-                .expect("failed to execute process");
-            let output = String::from_utf8(output.stdout).unwrap();
-            let mut buffer = String::new();
-            let mut file = std::fs::File::open("tests/command_output/list.txt").unwrap();
-            file.read_to_string(&mut buffer).unwrap();
-            assert_eq!(output, buffer);
-        }
-    }
-
-    #[test]
-    fn test_remove_help() {
-        if !cfg!(target_os = "windows") {
-            let output = Command::new("sh")
-                .arg("-c")
-                .arg("cargo run -- help remove")
-                .output()
-                .expect("failed to execute process");
-            let output = String::from_utf8(output.stdout).unwrap();
-            let mut buffer = String::new();
-            let mut file = std::fs::File::open("tests/command_output/remove.txt").unwrap();
-            file.read_to_string(&mut buffer).unwrap();
-            assert_eq!(output, buffer);
-        }
-    }
 }
---
+++ src/test.rs
@@ -0,0 +1,48 @@
+use std::{io::Read, process::Command};
+#[test]
+fn test_help() {
+    if !cfg!(target_os = "windows") {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("cargo run -- -h")
+            .output()
+            .expect("failed to execute process");
+        let output = String::from_utf8(output.stdout).unwrap();
+        let mut buffer = String::new();
+        let mut file = std::fs::File::open("tests/command_output/help.txt").unwrap();
+        file.read_to_string(&mut buffer).unwrap();
+        assert_eq!(output, buffer);
+    }
+}
+
+#[test]
+fn test_list_help() {
+    if !cfg!(target_os = "windows") {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("cargo run -- help list")
+            .output()
+            .expect("failed to execute process");
+        let output = String::from_utf8(output.stdout).unwrap();
+        let mut buffer = String::new();
+        let mut file = std::fs::File::open("tests/command_output/list.txt").unwrap();
+        file.read_to_string(&mut buffer).unwrap();
+        assert_eq!(output, buffer);
+    }
+}
+
+#[test]
+fn test_remove_help() {
+    if !cfg!(target_os = "windows") {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("cargo run -- help remove")
+            .output()
+            .expect("failed to execute process");
+        let output = String::from_utf8(output.stdout).unwrap();
+        let mut buffer = String::new();
+        let mut file = std::fs::File::open("tests/command_output/remove.txt").unwrap();
+        file.read_to_string(&mut buffer).unwrap();
+        assert_eq!(output, buffer);
+    }
+}
