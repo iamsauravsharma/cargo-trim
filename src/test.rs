b44d139b53262a1aa599b1c3cfa76ae0a5926de8
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-12-06 19:34:36 +0545

    Solve test error and add new test

--- src/test.rs
+++ src/test.rs
@@ -46,3 +46,19 @@ fn test_remove_help() {
         assert_eq!(output, buffer);
     }
 }
+
+#[test]
+fn test_query_help() {
+    if !cfg!(target_os = "windows") {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("cargo run -- help query")
+            .output()
+            .expect("failed to execute process");
+        let output = String::from_utf8(output.stdout).unwrap();
+        let mut buffer = String::new();
+        let mut file = std::fs::File::open("tests/command_output/query.txt").unwrap();
+        file.read_to_string(&mut buffer).unwrap();
+        assert_eq!(output, buffer);
+    }
+}
