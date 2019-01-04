6fcbae8e66266c3c2235b07802d8a8541565979b
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-01-04 19:37:58 +0545

    try integration test on windows through travis

--- src/test.rs
+++ src/test.rs
@@ -1,64 +1,68 @@
 use std::{io::Read, process::Command};
 #[test]
 fn test_help() {
-    if !cfg!(target_os = "windows") {
-        let output = Command::new("sh")
-            .arg("-c")
-            .arg("cargo run -- trim -h")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8(output.stdout).unwrap();
-        let mut buffer = String::new();
-        let mut file = std::fs::File::open("tests/command_output/help.txt").unwrap();
-        file.read_to_string(&mut buffer).unwrap();
-        assert_eq!(output, buffer);
-    }
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("cargo run -- trim -h")
+        .output()
+        .expect("failed to execute process");
+    let output = String::from_utf8(output.stdout).unwrap();
+    let mut buffer = String::new();
+    let mut file = std::fs::File::open("tests/command_output/help.txt").unwrap();
+    file.read_to_string(&mut buffer).unwrap();
+    assert_eq!(
+        output.replace("\r", "").replace("\n", ""),
+        buffer.replace("\r", "").replace("\n", "")
+    );
 }
 
 #[test]
 fn test_list_help() {
-    if !cfg!(target_os = "windows") {
-        let output = Command::new("sh")
-            .arg("-c")
-            .arg("cargo run -- trim help list")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8(output.stdout).unwrap();
-        let mut buffer = String::new();
-        let mut file = std::fs::File::open("tests/command_output/list.txt").unwrap();
-        file.read_to_string(&mut buffer).unwrap();
-        assert_eq!(output, buffer);
-    }
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("cargo run -- trim help list")
+        .output()
+        .expect("failed to execute process");
+    let output = String::from_utf8(output.stdout).unwrap();
+    let mut buffer = String::new();
+    let mut file = std::fs::File::open("tests/command_output/list.txt").unwrap();
+    file.read_to_string(&mut buffer).unwrap();
+    assert_eq!(
+        output.replace("\r", "").replace("\n", ""),
+        buffer.replace("\r", "").replace("\n", "")
+    );
 }
 
 #[test]
 fn test_remove_help() {
-    if !cfg!(target_os = "windows") {
-        let output = Command::new("sh")
-            .arg("-c")
-            .arg("cargo run -- trim help remove")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8(output.stdout).unwrap();
-        let mut buffer = String::new();
-        let mut file = std::fs::File::open("tests/command_output/remove.txt").unwrap();
-        file.read_to_string(&mut buffer).unwrap();
-        assert_eq!(output, buffer);
-    }
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("cargo run -- trim help remove")
+        .output()
+        .expect("failed to execute process");
+    let output = String::from_utf8(output.stdout).unwrap();
+    let mut buffer = String::new();
+    let mut file = std::fs::File::open("tests/command_output/remove.txt").unwrap();
+    file.read_to_string(&mut buffer).unwrap();
+    assert_eq!(
+        output.replace("\r", "").replace("\n", ""),
+        buffer.replace("\r", "").replace("\n", "")
+    );
 }
 
 #[test]
 fn test_query_help() {
-    if !cfg!(target_os = "windows") {
-        let output = Command::new("sh")
-            .arg("-c")
-            .arg("cargo run -- trim help query")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8(output.stdout).unwrap();
-        let mut buffer = String::new();
-        let mut file = std::fs::File::open("tests/command_output/query.txt").unwrap();
-        file.read_to_string(&mut buffer).unwrap();
-        assert_eq!(output, buffer);
-    }
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("cargo run -- trim help query")
+        .output()
+        .expect("failed to execute process");
+    let output = String::from_utf8(output.stdout).unwrap();
+    let mut buffer = String::new();
+    let mut file = std::fs::File::open("tests/command_output/query.txt").unwrap();
+    file.read_to_string(&mut buffer).unwrap();
+    assert_eq!(
+        output.replace("\r", "").replace("\n", ""),
+        buffer.replace("\r", "").replace("\n", "")
+    );
 }
