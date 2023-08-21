db2a9895363905c3f24a143b67eac29f80a8ddc0
Saurav Sharma <appdroiddeveloper@gmail.com>
2023-08-21 14:43:43 +0545

    test: move all help command test to single function
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- tests/command_output.rs
+++ tests/command_output.rs
@@ -16,40 +16,10 @@ fn run_cargo_trim(args: &[&str]) {
 #[test]
 fn test_help() {
     run_cargo_trim(&["help"]);
-}
-
-// test check list subcommand help
-#[test]
-fn test_list_help() {
     run_cargo_trim(&["help", "list"]);
-}
-
-// test check config subcommand help
-#[test]
-fn test_config_help() {
     run_cargo_trim(&["help", "config"]);
-}
-
-// test check git subcommand help
-#[test]
-fn test_git_help() {
     run_cargo_trim(&["help", "git"]);
-}
-
-// test check registry subcommand help
-#[test]
-fn test_registry_help() {
     run_cargo_trim(&["help", "registry"]);
-}
-
-// test check set subcommand help
-#[test]
-fn test_set_help() {
     run_cargo_trim(&["help", "set"]);
-}
-
-// test check unset subcommand help
-#[test]
-fn test_unset_help() {
     run_cargo_trim(&["help", "unset"]);
 }
